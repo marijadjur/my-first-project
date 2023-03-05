2a20e3bce7b9131f4b57e5ebea37dd1cfed0e7a2
marijadjur <djurmaria@gmail.com>
2023-03-05 23:02:09 +0200

    [homework_01032023]: Add homework progress

---
+++ src/main/java/lv/acodemy/homework_01032023/FirstHomeWork_01032023.java
@@ -0,0 +1,102 @@
+package lv.acodemy.homework_01032023;
+
+public class FirstHomeWork_01032023 {
+    public static void main(String[] args) {
+
+        //byte
+        byte onWhichFloorILive = 1;
+        System.out.println(onWhichFloorILive);
+
+        byte myCatAge = 12;
+        System.out.println(myCatAge);
+
+        //short
+        short riverAmazonLengthKm = 6400;
+        System.out.println(riverAmazonLengthKm);
+
+        short hermesBagPrice= 25000;
+        System.out.println(hermesBagPrice);
+
+        //char
+        char riverAmazonFirstLetter = 65; // 'A'
+        System.out.println(riverAmazonFirstLetter);
+
+        char myCatNameFirstLetter = 90; // 'Z'
+        System.out.println(myCatNameFirstLetter);
+
+        //int
+        int dateOfWedding = 26082016;
+        System.out.println(dateOfWedding);
+
+        int priceOfTheCar = 122335566;
+        System.out.println(priceOfTheCar);
+
+        //long
+        long populationOfEstonia = 1330000;
+        System.out.println(populationOfEstonia);
+
+        long populationOfLithuania = 2800000;
+        System.out.println(populationOfLithuania);
+
+        //float
+        float hugePositive = 32.100f;
+        System.out.println(hugePositive);
+
+        float hugeNegative= -32.100f;
+        System.out.println(hugeNegative);
+
+        //double
+        double cardBalance = 102.35;
+        System.out.println(cardBalance);
+
+        double receipt = 51.96;
+        System.out.println(receipt);
+
+        //boolean
+        boolean is2023 = true;
+        boolean is1995 = false;
+        boolean hasCandy = false;
+
+        //String
+        String city = "Riga";
+        String country = "Latvia";
+
+        //Concatenation
+        System.out.printf("My name is Maria.");
+        System.out.printf("I am 30 years old.");
+        System.out.printf("I love chocolate and traveling.");
+
+        //*
+        int a = 50;
+        int b = 147;
+        int c = a * b;
+        System.out.println(c);
+        System.out.println(50 * 147);
+        System.out.println(a * b);
+
+        // / division
+        double result = (double) a / b;
+        System.out.println(result);
+
+        // %
+        int number = 56 % 6;
+        System.out.println(number);
+
+        // +
+        int totalIncome = 2000 + 650;
+        System.out.println(totalIncome);
+
+        //-
+        int totalDistance = 15984 - 569;
+        System.out.println(totalDistance);
+
+
+
+
+
+
+
+
+
+    }
+}
