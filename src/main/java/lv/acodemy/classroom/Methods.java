0d590bf37d2dccf672fc04ee799364b1f9251ed2
marijadjur <djurmaria@gmail.com>
2023-03-09 20:49:37 +0200

    [classroom_09032023]: Add classroom progress

---
+++ src/main/java/lv/acodemy/classroom/Methods.java
@@ -0,0 +1,15 @@
+package lv.acodemy.classroom;
+
+public class Methods {
+    public static void main(String[] args) {
+        printText("My name is Maria", 5);
+    }
+
+    public static void printText(String text, int loopCount) {
+        for (int i = 0; i < loopCount; i++) {
+            System.out.println(text);
+        }
+
+            }
+        }
+    
